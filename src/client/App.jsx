e443888577e3bf6bf06fb80697c1cca1bbe4ee68
rhiroy <rhiroy@protonmail.ch>
2017-09-18 08:08:31 -0500

    added google maps

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import {GOOGLE_API_KEY} from '../config';
 
-const App = (props) => (
-    <div>
-        <p>Hello World</p>
-    </div>
-);
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    };
+
+    render() {
+        return (
+            <div>
+                <iframe width="1776" height="1375" frameborder="0" style={{border:0}}
+                        src={`https://www.google.com/maps/embed/v1/view?zoom=10&center=30.2672,-97.7431&key=${GOOGLE_API_KEY}`} allowfullscreen></iframe>
+            </div>
+        );
+    }
+
+}
+
+
 
 export default App;
 
